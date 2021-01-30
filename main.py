92406981ef04b378d2ee5dcff609bcea3db71828
sarveshcodes <70506426+sarveshcodes@users.noreply.github.com>
2021-01-30 13:32:19 +0530

    Update main.blocks, main.ts, main.py, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+def on_right_pressed():
+    animation.run_image_animation(mySprite, assets.animation("""
+        anim
+    """), 100, False)
+controller.right.on_event(ControllerButtonEvent.PRESSED, on_right_pressed)
 
+mySprite: Sprite = None
+mySprite = sprites.create(assets.image("""
+    image
+"""), SpriteKind.player)
+controller.move_sprite(mySprite)
+scene.set_background_color(7)
--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.vy = -120
+})
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
     mySprite,
@@ -7,6 +10,9 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     )
 })
 let mySprite: Sprite = null
-mySprite = sprites.create(assets.image`image`, SpriteKind.Player)
-controller.moveSprite(mySprite)
-scene.setBackgroundColor(7)
+mySprite = sprites.create(assets.image`image0`, SpriteKind.Player)
+scene.cameraFollowSprite(mySprite)
+controller.moveSprite(mySprite, 100, 0)
+scene.setBackgroundColor(9)
+tiles.setTilemap(tilemap`level1`)
+mySprite.ay = 250
