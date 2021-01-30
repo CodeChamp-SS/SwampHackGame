06cbdab7e872623da218ffeec6f0c13ae586acfc
Sayan Chatterjee <58657069+cyan-chatter@users.noreply.github.com>
2021-01-30 19:23:20 +0530

    few obstacles

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "boulder":return img`
+. . . . a a a a a a a . . . . . 
+. . a a e b b b b e e a a e . . 
+. a e e e b b b e e e e e a e . 
+. a e b b b b b 2 e e e e a e . 
+e e e b b b b b e e e e e e a e 
+a e 2 b b b b b 2 e e e e e a e 
+a e e b b b b b e e e e e e a e 
+a e 2 b b b b b b a a e e a a e 
+a e e e b b b b b b b b b b a e 
+a e e 2 b b b b b b b b b b a e 
+a e e e e 2 e 2 e b b b b b e . 
+e a e e e e e e e b b b b a e . 
+. a 2 e e e e e e b b b b a e . 
+. e a a 2 e 2 e 2 b b a a e e . 
+. . e e a a a a e e 2 e e e . . 
+. . e e e e e e e e e e e . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
