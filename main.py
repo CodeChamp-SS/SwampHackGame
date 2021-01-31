75549ef321e6da2382013e3c17062142421deb69
sarveshcodes <70506426+sarveshcodes@users.noreply.github.com>
2021-01-31 15:26:00 +0530

    Update main.blocks, main.ts, images.g.jres, images.g.ts, main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ controller.up.on_event(ControllerButtonEvent.PRESSED, on_up_pressed)
 def on_on_overlap(sprite, coll1):
     coll1.destroy()
     info.change_life_by(-1)
+    statusbar.value += -30
 sprites.on_overlap(SpriteKind.player, SpriteKind.projectile, on_on_overlap)
 
 def on_overlap_tile(sprite, location):
@@ -28,7 +29,45 @@ scene.on_hit_wall(SpriteKind.projectile, on_hit_wall)
 
 coll1: Sprite = None
 jumpcount = 0
+statusbar: StatusBarSprite = None
 jack: Sprite = None
+game.set_dialog_frame(img("""
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+"""))
+game.set_dialog_text_color(5)
+game.set_dialog_cursor(img("""
+    ..............................
+        ..............................
+        ..............................
+        777777777777777777777777......
+        7......................7......
+        7.111.111.1111.111.111.7......
+        7.1...1.1.1..1.1...1...7......
+        7.111.111.1..1.1...111.7......
+        7...1.1...1111.1...1...7......
+        7.111.1...1..1.111.111.7......
+        7......................7......
+        777777777777777777777777......
+        ..............................
+        ..............................
+        ..............................
+"""))
+game.show_long_text("Welcome To The world of retro.\\n Try to win as fast as possible.\\n press space to START GAME",
+    DialogLayout.CENTER)
 scene.set_background_color(9)
 tiles.set_tilemap(tilemap("""
     level4
@@ -42,7 +81,7 @@ info.set_life(3)
 jack.ay = 250
 scene.camera_follow_sprite(jack)
 statusbar = statusbars.create(20, 4, StatusBarKind.energy)
-statusbar.value = 10
+statusbar.max = 100
 statusbar.position_direction(CollisionDirection.LEFT)
 statusbar.set_offset_padding(-40, 0)
 test = sprites.all_of_kind(SpriteKind.player)
@@ -63,7 +102,7 @@ def on_update_interval():
     global coll1
     if game.runtime() > 3000:
         coll1 = sprites.create_projectile_from_side(assets.image("""
-            image5
+            image8
         """), -39, 50)
         coll1.set_position(jack.x + 50, jack.y - 50)
 game.on_update_interval(2000, on_update_interval)
